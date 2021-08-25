9bc9b1c82f0843c51071df2c16af327aed315a8e
Yang <yywqdu@163.com>
2021-08-25 20:52:57 +0800

    更改类名TankGame03 -> TankGame04

---
+++ src/com/yyw/tank04/TankGame04.java
@@ -0,0 +1,29 @@
+package com.yyw.tank04;
+
+import javax.swing.*;
+
+/**
+ * @author yywqd
+ */
+public class TankGame04 extends JFrame {
+    MyPanel mp;
+
+    public TankGame04() {
+        mp = new MyPanel();
+        // 启动线程
+        new Thread(mp).start();
+
+        this.add(mp);
+        // 窗口JFrame 对象监听键盘事件
+        this.addKeyListener(mp);
+        this.setSize(1100, 850);
+        this.setVisible(true);
+        //  点击右上角的关闭按钮，退出进程
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new TankGame04();
+    }
+
+}
